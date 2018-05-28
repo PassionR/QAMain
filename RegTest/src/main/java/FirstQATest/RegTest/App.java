f28f912739a987eaf9849e954b453d075608c5b1
freena <freena@10.0.0.214>
2018-05-28 14:12:14 -0600

    commit

---
+++ RegTest/src/main/java/FirstQATest/RegTest/App.java
@@ -0,0 +1,13 @@
+package FirstQATest.RegTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
